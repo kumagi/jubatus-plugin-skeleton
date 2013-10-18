04d950bb4c397692ba58d362f65a455b85797977
udagawa <udagawa.takuro@lab.ntt.co.jp>
2013-10-18 18:25:12 +0900

    added binary skeleton and organized directory

--- src/binary_length.hpp
+++
@@ -1,6 +0,0 @@
-#include <jubatus/core/plugin.hpp>
-
-class binary_converter : public jubatus::core::fv_converter::binary_feature{
-public:
-  void add_feature(const std::string& key, const std::string& value, std::vector< std::pair< std::string, float > >& ret_fv)) const;
-}
