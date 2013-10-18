30b5ad5b1089c8e6392c2112e4b1ffaa9b80abf1
Hiroki KUMAZAKI <rintyo_@hotmail.com>
2013-10-18 23:08:57 +0900

    Jubatusコード規約
    関数の引数を折り返す時は第一引数から全てを4字下げで各行インデントする。
    )と{の間はスペース1つ入れる。

--- src/binary_skeleton/binary_length.cpp
+++ src/binary_skeleton/binary_length.cpp
@@ -3,14 +3,17 @@
 #include <string>
 #include <iostream>
 
-void binary_converter::add_feature(const std::string& key, const std::string& value, 
-				   std::vector< std::pair< std::string, float > >& ret_fv) const {
+void binary_converter::add_feature(
+    const std::string& key,
+    const std::string& value, 
+    std::vector<std::pair<std::string, float> >& ret_fv) const {
   std::cout << "length =" << value.length() << std::endl;
   ret_fv.push_back(make_pair(key, (float)  value.length()));
 }
 
 extern "C"{
-  jubatus::core::fv_converter::binary_feature* create(std::map <std::string, std::string>& params){
+  jubatus::core::fv_converter::binary_feature* create(
+      std::map <std::string, std::string>& params) {
     return new binary_converter();
   }
 }
